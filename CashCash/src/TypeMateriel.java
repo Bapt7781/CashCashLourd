8bd10c67e94d1f000b0c58ef934001ed76605497
Baptiste Delisle <baptiste.delisle@gmail.com>
2024-03-27 14:12:16 +0100

    materiel + type

--- CashCash/src/TypeMateriel.java
+++ CashCash/src/TypeMateriel.java
@@ -1,3 +1,37 @@
 public class TypeMateriel {
-     
+    private String referenceInterne;
+    private String libelleTypeMateriel;
+    private Famille laFamille;
+
+    // Constructeur
+    public TypeMateriel(String referenceInterne, String libelleTypeMateriel, Famille laFamille) {
+        this.referenceInterne = referenceInterne;
+        this.libelleTypeMateriel = libelleTypeMateriel;
+        this.laFamille = laFamille;
+    }
+
+    // Getters et setters
+    public String getReferenceInterne() {
+        return referenceInterne;
+    }
+
+    public void setReferenceInterne(String referenceInterne) {
+        this.referenceInterne = referenceInterne;
+    }
+
+    public String getLibelleTypeMateriel() {
+        return libelleTypeMateriel;
+    }
+
+    public void setLibelleTypeMateriel(String libelleTypeMateriel) {
+        this.libelleTypeMateriel = libelleTypeMateriel;
+    }
+
+    public Famille getLaFamille() {
+        return laFamille;
+    }
+
+    public void setLaFamille(Famille laFamille) {
+        this.laFamille = laFamille;
+    }
 }
