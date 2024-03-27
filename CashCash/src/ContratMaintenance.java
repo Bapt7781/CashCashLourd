b2ffcd3472cfc683a2879840bd0665ba44084eb9
Martin Amblanc <amblancm@gmail.com>
2024-03-27 14:49:12 +0100

    nouveau

--- CashCash/src/ContratMaintenance.java
+++ CashCash/src/ContratMaintenance.java
@@ -1,3 +1,44 @@
+import java.sql.Date;
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class ContratMaintenance {
-    
+private String numContrat;
+private Date dateSignature, dateEcheance;
+private ArrayList<Materiel> lesMaterielsAssures = new ArrayList<>();
+
+public ContratMaintenance(String numContrat, Date dateSignature, Date dateEcheance) {
+        this.numContrat = numContrat;
+        this.dateSignature = dateSignature;
+        this.dateEcheance = dateEcheance;
+        this.lesMaterielsAssures = new ArrayList<>();
+    }
+    public int getJoursRestants() {
+        
+    }
+    public boolean estValide() {
+    }
+    public void ajouteMateriel(Materiel unMateriel) {
+        lesMaterielsAssures.add(unMateriel);
+    }
+
+    // Méthode pour obtenir le numéro de contrat
+    public String getNumContrat() {
+        return numContrat;
+    }
+
+    // Méthode pour obtenir la date de signature
+    public Date getDateSignature() {
+        return dateSignature;
+    }
+
+    // Méthode pour obtenir la date d'échéance
+    public Date getDateEcheance() {
+        return dateEcheance;
+    }
+
+    // Méthode pour obtenir les matériels assurés
+    public Collection<Materiel> getLesMaterielsAssures() {
+        return lesMaterielsAssures;
+    }
 }
--- CashCash/src/Famille.java
+++ CashCash/src/Famille.java
@@ -1,3 +1,25 @@
 public class Famille {
+    private String codeFamille,libelleFamille;
     
+    public Famille(String codeFamille, String libelleFamille) {
+        this.codeFamille = codeFamille;
+        this.libelleFamille = libelleFamille;
+    }
+
+    public String getCodeFamille() {
+        return codeFamille;
+    }
+
+    public void setCodeFamille(String codeFamille) {
+        this.codeFamille = codeFamille;
+    }
+
+    public String getLibelleFamille() {
+        return libelleFamille;
+    }
+
+    public void setLibelleFamille(String libelleFamille) {
+        this.libelleFamille = libelleFamille;
+    }
 }
+
