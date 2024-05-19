0f26f7a4837b5819b32305988688b5c449d1a7da
Martin Amblanc <amblancm@gmail.com>
2024-05-19 18:25:26 +0200

    ertghyn

--- CashCash/src/PersistanceSQL.java
+++ CashCash/src/PersistanceSQL.java
@@ -261,8 +261,7 @@ public class PersistanceSQL {
         }
         return clients;
     }
-
-    public void generationPDF30jours(){ 
+    public void generationPDF30jours(){
         System.out.println("qzzqdh");
         int nbjour = 30;
         ArrayList<Client> TousClient = recupererClientsPourRelance(nbjour);
@@ -273,21 +272,40 @@ public class PersistanceSQL {
             try (PDDocument document = new PDDocument()) {
                 PDPage page = new PDPage();
                 document.addPage(page);
-                
+               
                 // Création d'un flux de contenu pour la page
                 try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
                     // Début du texte
                     contentStream.beginText();
-                    // Choisir la police et la taille
-                    contentStream.setFont(PDType1Font.COURIER, 12);
+                    // Choisir la police et la taille pour la première ligne
+                    contentStream.setFont(PDType1Font.COURIER, 20);
                     // Déplacer le curseur à une position x, y
-                    contentStream.newLineAtOffset(100, 700);
-                    // Ajouter du texte
-                    contentStream.showText("Boujour " + unClient.getRaisonSociale()+ " Nous vous rapellons que votre contrat de maintenance arrive a échéance à la date du: " + unClient.getDateEcheance()+ " Merci de revenir vers nous pour renouveler votre contrat. Cordialement votre équipe d'assistance CashCash.");
+                    contentStream.newLineAtOffset(50, 700);
+                    contentStream.setLeading(14.5f);
+                    // Ajouter du texte pour la première ligne
+                    contentStream.showText("Boujour " + unClient.getRaisonSociale());
+                    // Retour à la ligne
+                    contentStream.newLine();
+               
+                    // Changer la police et la taille pour la deuxième ligne
+                    contentStream.setFont(PDType1Font.COURIER, 9);
+                    // Ajouter du texte pour la deuxième ligne
+                    contentStream.showText("Nous vous rappelons que votre contrat de maintenance arrive a échéance à la date du: " + unClient.getDateEcheance());
+                    // Retour à la ligne
+                    contentStream.newLine();
+               
+                    // Ajouter du texte pour la troisième ligne
+                    contentStream.showText("Merci de revenir vers nous pour renouveler votre contrat.");
+                    contentStream.newLine();
+ 
+                    contentStream.showText("Cordialement votre équipe d'assistance CashCash.");
+ 
+ 
                     // Fin du texte
                     contentStream.endText();
                 }
-                
+               
+               
                 // Sauvegarder le document en tant que fichier PDF
                 document.save(dest);
                 JOptionPane.showMessageDialog(null, "Fichier PDF créer", "Succès",
@@ -296,7 +314,7 @@ public class PersistanceSQL {
                 e.printStackTrace();
             }
         }
-    }
+        }
     public void generationPDF15jours(){ 
         System.out.println("qzzqdh");
         int nbjour = 15;
