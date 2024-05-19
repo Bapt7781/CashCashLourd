8ac474f248270e71488568b1cd1d6f62dba5742e
Martin Amblanc <amblancm@gmail.com>
2024-05-19 18:00:05 +0200

    fonctionaliter envoie mail

--- CashCash/src/PersistanceSQL.java
+++ CashCash/src/PersistanceSQL.java
@@ -297,4 +297,74 @@ public class PersistanceSQL {
             }
         }
     }
+    public void generationPDF15jours(){ 
+        System.out.println("qzzqdh");
+        int nbjour = 15;
+        ArrayList<Client> TousClient = recupererClientsPourRelance(nbjour);
+        for(Client unClient : TousClient){
+            System.out.println(unClient.getNumClient());
+            String dest = unClient.getRaisonSociale() + "Rappel30Jours.pdf";
+            System.out.println(dest);
+            try (PDDocument document = new PDDocument()) {
+                PDPage page = new PDPage();
+                document.addPage(page);
+                
+                // Création d'un flux de contenu pour la page
+                try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
+                    // Début du texte
+                    contentStream.beginText();
+                    // Choisir la police et la taille
+                    contentStream.setFont(PDType1Font.COURIER, 12);
+                    // Déplacer le curseur à une position x, y
+                    contentStream.newLineAtOffset(100, 700);
+                    // Ajouter du texte
+                    contentStream.showText("Boujour " + unClient.getRaisonSociale()+ " Nous vous rapellons que votre contrat de maintenance arrive a échéance à la date du: " + unClient.getDateEcheance()+ " Merci de revenir vers nous pour renouveler votre contrat. Cordialement votre équipe d'assistance CashCash.");
+                    // Fin du texte
+                    contentStream.endText();
+                }
+                
+                // Sauvegarder le document en tant que fichier PDF
+                document.save(dest);
+                JOptionPane.showMessageDialog(null, "Fichier PDF créer", "Succès",
+                    JOptionPane.INFORMATION_MESSAGE);
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public void generationPDF3jours(){ 
+        System.out.println("qzzqdh");
+        int nbjour = 3;
+        ArrayList<Client> TousClient = recupererClientsPourRelance(nbjour);
+        for(Client unClient : TousClient){
+            System.out.println(unClient.getNumClient());
+            String dest = unClient.getRaisonSociale() + "Rappel30Jours.pdf";
+            System.out.println(dest);
+            try (PDDocument document = new PDDocument()) {
+                PDPage page = new PDPage();
+                document.addPage(page);
+                
+                // Création d'un flux de contenu pour la page
+                try (PDPageContentStream contentStream = new PDPageContentStream(document, page)) {
+                    // Début du texte
+                    contentStream.beginText();
+                    // Choisir la police et la taille
+                    contentStream.setFont(PDType1Font.COURIER, 12);
+                    // Déplacer le curseur à une position x, y
+                    contentStream.newLineAtOffset(100, 700);
+                    // Ajouter du texte
+                    contentStream.showText("Boujour " + unClient.getRaisonSociale()+ " Nous vous rapellons que votre contrat de maintenance arrive a échéance à la date du: " + unClient.getDateEcheance()+ " Merci de revenir vers nous pour renouveler votre contrat. Cordialement votre équipe d'assistance CashCash.");
+                    // Fin du texte
+                    contentStream.endText();
+                }
+                
+                // Sauvegarder le document en tant que fichier PDF
+                document.save(dest);
+                JOptionPane.showMessageDialog(null, "Fichier PDF créer", "Succès",
+                    JOptionPane.INFORMATION_MESSAGE);
+            }catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
